ce6bb1909f2e411d6127ee4e6347ba89a85fe87e
Lindsay Kinn <lindsay.kinn@gmail.com>
2021-03-11 12:32:35 -0500

    commit tree 5b284a3c3e9c6cd0c481c8b8f76d7865ef54ad39

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(name, string){
+  return name.filter(match => match.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string){
+  return array.filter(match => match.toLowerCase().indexOf(string.toLowerCase()) === 0)
+}
+
+function matchName(array, string){
+  return array.filter(something => something.name === string)
+}
